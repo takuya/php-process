921c8084b2a7035566153d2b5ca553f475eb67f5
takuya <takuya.1st+nospam@gmail.com>
2020-03-16 17:08:14 +0900

    checking stderr pipe limit added.

--- samples/src/check_available_max_pipe_size.php
+++ samples/src/check_available_max_pipe_size.php
@@ -2,21 +2,51 @@
 
 use SystemUtil\Process;
 
-require_once '../../src/Process.php';
+require_once __DIR__.'/../../src/Process.php';
 
 
-
-foreach ( range( 256*256-10,256*257) as $i){
-  $size = $i;
-  $proc = new Process(['head', '-c', $size, '/dev/urandom']);
-  $proc->setTimeout(1);
-  $proc->run();
-  if($proc->canceled()) {
-    echo "{$size}, failed .\n";
-    printf("It is %d , seems to be max limit for proc_open [1=>['pipe'=>'w']].\n", $size-1);
-    break;
-  }else{
-    echo "{$size}, ok .\n";
+function checkPipe1(){
+  printf("-- checking stdout.\n");
+  foreach ( range( 256*256-4,256*257) as $i){
+    $size = $i;
+    $proc = new Process(['head', '-c', $size, '/dev/urandom']);
+    $proc->setTimeout(1);
+    $proc->run();
+    if($proc->canceled()) {
+      echo "{$size}, failed .\n";
+      printf("It is %d , seems to be max limit for proc_open [1=>['pipe'=>'w']].\n", $size-1);
+      break;
+    }else{
+      echo "{$size}, ok.\n";
+    }
   }
   
 }
+function checkPipe2(){
+  printf("-- checking stderr.\n");
+  foreach ( range( 256*256-4,256*257) as $i){
+    $size = $i;
+    $proc = new Process('php');
+    $proc->setInput(sprintf('<?php
+    $fd=fopen("php://stderr","w+");
+    foreach( range(0,%d) as $i ){ fwrite($fd, "1"); };
+    fflush($fd);
+    fclose($fd);
+    ;
+  ',$size));
+    $proc->setTimeout(1);
+    $proc->run();
+    if($proc->canceled()) {
+      echo "{$size}, failed .\n";
+      printf("It is %d , seems to be max limit for proc_open [2=>['pipe'=>'w']].\n", $size-1);
+      break;
+    }else{
+      $out = $proc->getErrout();
+      printf ("write %d bytes, error output %d bytes.\n", $size, fstat($out)['size']);
+    }
+  }
+}
+
+
+checkPipe1();
+checkPipe2();
